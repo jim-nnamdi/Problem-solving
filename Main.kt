5470b3ad491c21889b744bee4fbcdbfacfb84b0b
jim-nnamdi <jimsamuel50@gmail.com>
2022-11-05 14:50:43 +0100

    number steps

---
+++ Main.kt
@@ -0,0 +1,28 @@
+fun sequenceSteps(arrData: Array<Int>, maxNo:Int, steps: Int): Int{
+
+    for(item in arrData) {
+        // check that no item in the array is
+        // greater than the maxNo
+        if (item > maxNo) {
+            // run another iteration
+            // display notice before that
+            println("the max number we can have is $maxNo")
+            var currSteps = 0
+            for(num in 0..maxNo){
+
+                // check if num is divisible by step
+                // then return the corresponding values
+                // using the modulo function
+                if(num % steps == 0 ) {
+                    println(num)
+                }
+            }
+        }
+    }
+    return 0
+}
+
+fun main(){
+    var result = sequenceSteps(arrayOf(2,3,4,5), 4, 3)
+    println(result)
+}
